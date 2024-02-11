151c403c568b73f1a33bc54e071169d993fc93cd
Jonathan Kulak <lackadaize@msn.com>
2024-02-11 13:55:55 -0500

    Basic HTMLand CSS established. API fetch console.log successful.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function loadCharacters() {
+    // fetch(`http://localhost:3000/results`)
+    fetch(`https://swapi.dev/api/people`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+        // ul.innerHTML = JSON.stringify(data) 
+    })
+}
 
+function filterCharacters() {
+    console.log("Hello World!")
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    loadCharacters()
+    filterCharacters()
+})
