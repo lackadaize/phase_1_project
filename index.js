8cda17bd0e6d8394c47a58a130e2a9c71623c524
Jonathan Kulak <lackadaize@msn.com>
2024-02-11 16:10:53 -0500

    Initial attempt to fetch and append data from external API semi-successful.

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
-function loadCharacters() {
+// Global variables
+filmAll = "https://swapi.dev/api/people"
+// film1 = "films": https://swapi.dev/api/films/1
+// film2 = "films": https://swapi.dev/api/films/2
+// film3 = "films": https://swapi.dev/api/films/3
+// film4 = "films": https://swapi.dev/api/films/4
+// film5 = "films": https://swapi.dev/api/films/5
+// film6 = "films": https://swapi.dev/api/films/6
+function loadCharacters(film) {
     // fetch(`http://localhost:3000/results`)
     fetch(`https://swapi.dev/api/people`)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data)
-        // ul.innerHTML = JSON.stringify(data) 
-    })
-}
+      .then((response) => response.json())
+      .then((data) => {
+        let characterContainer = document.getElementById("character-container"); 
+        data.results.forEach(() => {
+          let characterCard = document.createElement("div"); 
+          characterCard.classList.add("character-card");
+          characterCard.innerHTML = `
+            <div>Name: ${data.results.name}<br/>Homeworld: `;
+          characterContainer.appendChild(characterCard); 
+        });
+        document.getElementById("character-container").appendChild(characterCard);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
-function filterCharacters() {
-    console.log("Hello World!")
-}
+// function filterCharacters() {
+//     const filmSelect = document.getElementById("film-select").value
+//     const characterContainer = document.getElementById("character-container")
+
+//     filmSelect.addEventListener("change", (event) => {
+//         event.preventDefault()
+//         console.log(characterContainer)
+//     })
+// }
 
 document.addEventListener("DOMContentLoaded", (event) => {
     loadCharacters()
-    filterCharacters()
+    // filterCharacters()
 })
