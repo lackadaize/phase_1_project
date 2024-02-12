dd040b4a0d2a1dff0f4e4c35e654bc172d443697
Jonathan Kulak <lackadaize@msn.com>
2024-02-12 12:31:04 -0500

    In the middle of attempting to get loadCharacters() to populate div#character-container.  I can populate single character-cards but not all at the same time.

--- index.js
+++ index.js
@@ -1,28 +1,66 @@
 // Global variables
-filmAll = "http://localhost:3000/characters"
-// filmAll = "https://swapi.dev/api/people"
-function loadCharacters(film) {
-    fetch(`http://localhost:3000/characters`)
-    // fetch(`https://swapi.dev/api/people`)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data)
-        let characterContainer = document.getElementById("character-container")
-        data.forEach((character) => {
-          let characterCard = document.createElement("div")
-          characterCard.classList.add("character-card")
-          characterCard.innerHTML = `
-            <div class="character-img"><img src="${character.image_url}" alt="${character.name} image"></div>
-            <div class="character-name">${character.name}</div>
-            `
-          characterContainer.appendChild(characterCard)
-        });
-        document.getElementById("character-container").appendChild(characterCard)
-      })
-      .catch((error) => {
-        console.error(error)
-      });
-  }
+const filmAll = "http://localhost:3000/characters"
+let filmAllArray = [];
+
+// Fetch array of all character objects
+function initialFetch() {
+
+    fetch(filmAll)
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data)
+            filmAllArray.push(data) // doesn't work when it's just (data) without [0]
+            console.log(filmAllArray)
+            loadCharacters(filmAllArray)
+        })
+        .catch((error) => {
+        console.log(error);
+    })
+}
+
+// Populate all characters on page load
+function loadCharacters(array) {
+    let characterContainer = document.getElementById("character-container")
+    array.forEach((character) => {
+        let characterCard = document.createElement("div")
+        characterCard.classList.add("character-card")
+        characterCard.innerHTML = 
+        `
+        <div class="character-img">
+            <img src="${character.image_url}" alt="${character.name} image">
+        </div>
+        <div class="character-name">${character.name}</div>
+        `
+        characterContainer.appendChild(characterCard)
+    })
+}
+
+// // Initial Character Load
+// function loadCharacters() {
+//     fetch(filmAll)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         filmAllArray = data
+//         let characterContainer = document.getElementById("character-container")
+//         data.forEach((character) => {
+//           let characterCard = document.createElement("div")
+//           characterCard.classList.add("character-card")
+//           characterCard.innerHTML = `
+//             <div class="character-img">
+//                 <img src="${character.image_url}" alt="${character.name} image">
+//             </div>
+//             <div class="character-name">${character.name}</div>
+//             `
+//           characterContainer.appendChild(characterCard)
+          
+//         })
+//       })
+//       .catch((error) => {
+//         console.error(error)
+//       })
+//   }
+
+
 
 // function filterCharacters() {
 //     const filmSelect = document.getElementById("film-select").value
@@ -35,6 +73,6 @@ function loadCharacters(film) {
 // }
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    loadCharacters()
+    initialFetch()
     // filterCharacters()
 })
