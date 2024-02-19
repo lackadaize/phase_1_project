78509e4bacc3ebce670379dd25bcdaf00c7f682a
Jonathan Kulak <lackadaize@msn.com>
2024-02-19 07:52:45 -0500

    use replaceChildren to delete 'character-container' content when 'film-select' is changed.

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 // Global variables
 const filmAll = "http://localhost:3000/characters" // fetch url
-let filmAllArray = [] // Array for characters in all films
-let filmOneArray = []
+let filmAllArray = [] // Array variable for characters in all films
+let filmCurrent // variable to be used to designate the film currently chosen 
+let filmOneArray = [] // The following array variables are for specific films
 let filmTwoArray = []
 let filmThreeArray = []
 let filmFourArray = []
 let filmFiveArray = []
 let filmSixArray = []
-let filmCurrent = []
 
-
-// Fetch array of all character objects
+// Fetch array of all character objects from db.json
 function initialFetch() {
     fetch(filmAll)
         .then(response => response.json())
         .then((data) => {
-            filmAllArray = data // replace array variables with 'data' response
+            filmAllArray = data 
             filmCurrent = filmAllArray
             filmOneArray = filmAllArray.filter((film) => film.films.includes("https://swapi.dev/api/films/1/"))
             filmTwoArray = filmAllArray.filter((film) => film.films.includes("https://swapi.dev/api/films/2/"))
@@ -23,7 +22,7 @@ function initialFetch() {
             filmFourArray = filmAllArray.filter((film) => film.films.includes("https://swapi.dev/api/films/4/"))
             filmFiveArray = filmAllArray.filter((film) => film.films.includes("https://swapi.dev/api/films/5/"))
             filmSixArray = filmAllArray.filter((film) => film.films.includes("https://swapi.dev/api/films/6/"))
-            loadCharacters(filmCurrent) // will need to pass a parameter/argument somewhere here
+            loadCharacters(filmCurrent) 
         })
         .catch((error) => {
         console.log(error);
@@ -33,7 +32,7 @@ function initialFetch() {
 // Populate characters on page load and when "film-select" value is changed
 function loadCharacters(filmCurrent) {
     let characterContainer = document.getElementById("character-container")
-    characterContainer.innerHTML = ''
+    characterContainer.replaceChildren() // Remove any current child content
     filmCurrent.forEach((character) => {
         let characterCard = document.createElement("div")
         characterCard.classList.add("character-card")
@@ -48,42 +47,33 @@ function loadCharacters(filmCurrent) {
     })
 }
 
-// remove()
-// removeChild()
-// replaceChildren()
-// innerHTML reset children of 'character-container' if time is of essence but insecure
-    //  State in READM.md
+// Filter characters based on select film chosen
+function filterCharacters() {
+    const filmSelect = document.getElementById("film-select");
+    filmSelect.addEventListener("change", (event) => {
+        event.preventDefault();
+        const selectedFilm = filmSelect.value;    
+        if (selectedFilm === "film-all") {
+        filmCurrent = filmAllArray;
+        } else if (selectedFilm === "film-one") {
+        filmCurrent = filmOneArray;
+        } else if (selectedFilm === "film-two") {
+        filmCurrent = filmTwoArray;
+        } else if (selectedFilm === "film-three") {
+        filmCurrent = filmThreeArray;
+        } else if (selectedFilm === "film-four") {
+        filmCurrent = filmFourArray;
+        } else if (selectedFilm === "film-five") {
+        filmCurrent = filmFiveArray;
+        } else if (selectedFilm === "film-six") {
+        filmCurrent = filmSixArray;
+        }      
+        loadCharacters(filmCurrent)
+    })
+}  
 
-    function filterCharacters() {
-        const filmSelect = document.getElementById("film-select");
-        filmSelect.addEventListener("change", (event) => {
-          event.preventDefault();
-          const selectedFilm = filmSelect.value;
-      
-          // Update filmCurrent based on the selected film
-          if (selectedFilm === "film-all") {
-            filmCurrent = filmAllArray;
-          } else if (selectedFilm === "film-one") {
-            filmCurrent = filmOneArray;
-          } else if (selectedFilm === "film-two") {
-            filmCurrent = filmTwoArray;
-          } else if (selectedFilm === "film-three") {
-            filmCurrent = filmThreeArray;
-          } else if (selectedFilm === "film-four") {
-            filmCurrent = filmFourArray;
-          } else if (selectedFilm === "film-five") {
-            filmCurrent = filmFiveArray;
-          } else if (selectedFilm === "film-six") {
-            filmCurrent = filmSixArray;
-          }
-      
-          loadCharacters(filmCurrent);
-        });
-      }  
-
+// Initial load
 document.addEventListener("DOMContentLoaded", (event) => {
     initialFetch()
     filterCharacters()
 })
-
-// Each of your event listeners should also have its own unique callback function!
