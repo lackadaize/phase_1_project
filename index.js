688698f40f09ce47e1d17bd69db09d6c7c1a049a
Jonathan Kulak <lackadaize@msn.com>
2024-08-20 11:13:58 -0400

    Rearrange tabs

--- index.js
+++ index.js
@@ -12,107 +12,107 @@ let characterSelected
 
 // Fetch array of all character objects from db.json
 const initialFetch = () => {
-    fetch(filmAll)
-        .then(response => response.json())
-        .then((data) => {
-            filmAllArray = data.sort(function(){return .5 - Math.random()}) 
-            filmCurrent = filmAllArray
-            filmOneArray = filmAllArray.filter((film) => film.films.includes("The Phantom Menace")).sort(function(){return .5 - Math.random()})
-            filmTwoArray = filmAllArray.filter((film) => film.films.includes("Attack of the Clones")).sort(function(){return .5 - Math.random()})
-            filmThreeArray = filmAllArray.filter((film) => film.films.includes("Revenge of the Sith")).sort(function(){return .5 - Math.random()})
-            filmFourArray = filmAllArray.filter((film) => film.films.includes("A New Hope")).sort(function(){return .5 - Math.random()})
-            filmFiveArray = filmAllArray.filter((film) => film.films.includes("The Empire Strikes Back")).sort(function(){return .5 - Math.random()})
-            filmSixArray = filmAllArray.filter((film) => film.films.includes("Return of the Jedi")).sort(function(){return .5 - Math.random()})
-            loadCharacters(filmCurrent) 
-        })
-        .catch((error) => console.log(error))
+  fetch(filmAll)
+    .then(response => response.json())
+    .then((data) => {
+      filmAllArray = data.sort(function(){return .5 - Math.random()}) 
+      filmCurrent = filmAllArray
+      filmOneArray = filmAllArray.filter((film) => film.films.includes("The Phantom Menace")).sort(function(){return .5 - Math.random()})
+      filmTwoArray = filmAllArray.filter((film) => film.films.includes("Attack of the Clones")).sort(function(){return .5 - Math.random()})
+      filmThreeArray = filmAllArray.filter((film) => film.films.includes("Revenge of the Sith")).sort(function(){return .5 - Math.random()})
+      filmFourArray = filmAllArray.filter((film) => film.films.includes("A New Hope")).sort(function(){return .5 - Math.random()})
+      filmFiveArray = filmAllArray.filter((film) => film.films.includes("The Empire Strikes Back")).sort(function(){return .5 - Math.random()})
+      filmSixArray = filmAllArray.filter((film) => film.films.includes("Return of the Jedi")).sort(function(){return .5 - Math.random()})
+      loadCharacters(filmCurrent) 
+    })
+    .catch((error) => console.log(error))
 }
 
 // Populate characters on page load and when "film-select" value is changed
 const loadCharacters = (filmCurrent) => {
-    let characterContainer = document.getElementById("characters-container")
-    characterContainer.replaceChildren() // Remove any current child content
-    filmCurrent.forEach((character) => { // Iterate over filmCurrent array to create elements on the front-end
-        let characterCard = document.createElement("div")
-        characterCard.setAttribute("class", "character-card")
-        characterCard.setAttribute("id", `character-${character.id}-card`)
-        characterCard.innerHTML = 
-            `
-            <div class="character-img">
-                <img src="${character.image_url}" alt="${character.name} image">
-            </div>
-            <div class="character-name">${character.name}</div>
-            <div id="character-${character.id}-info" class="character-info /*character-info-hide*/">
-                <button class="close-button">X</button>
-                <div>Species: ${character.species}</div>
-                <div>Homeworld: ${character.homeworld}</div>
-                <div>Birth Year: ${character.birth_year}</div>
-                <div>Height: ${character.height}</div>
-                <div>Mass: ${character.mass}</div>
-                <div>Hair Color: ${character.hair_color}</div>
-                <div>Skin Color: ${character.skin_color}</div>
-                <div>Eye Color: ${character.eye_color}</div>
-                <div>Films: ${character.films.join(", ")}</div>
-            </div>
-            `
-        characterContainer.appendChild(characterCard)
+  let characterContainer = document.getElementById("characters-container")
+  characterContainer.replaceChildren() // Remove any current child content
+  filmCurrent.forEach((character) => { // Iterate over filmCurrent array to create elements on the front-end
+    let characterCard = document.createElement("div")
+    characterCard.setAttribute("class", "character-card")
+    characterCard.setAttribute("id", `character-${character.id}-card`)
+    characterCard.innerHTML = 
+      `
+      <div class="character-img">
+        <img src="${character.image_url}" alt="${character.name} image">
+      </div>
+      <div class="character-name">${character.name}</div>
+      <div id="character-${character.id}-info" class="character-info /*character-info-hide*/">
+        <button class="close-button">X</button>
+        <div>Species: ${character.species}</div>
+        <div>Homeworld: ${character.homeworld}</div>
+        <div>Birth Year: ${character.birth_year}</div>
+        <div>Height: ${character.height}</div>
+        <div>Mass: ${character.mass}</div>
+        <div>Hair Color: ${character.hair_color}</div>
+        <div>Skin Color: ${character.skin_color}</div>
+        <div>Eye Color: ${character.eye_color}</div>
+        <div>Films: ${character.films.join(", ")}</div>
+      </div>
+      `
+    characterContainer.appendChild(characterCard)
         
-        // Click characterCard Events
-        let characterInfo = document.getElementById(`character-${character.id}-info`)
+    // Click characterCard Events
+    let characterInfo = document.getElementById(`character-${character.id}-info`)
+    characterInfo.style.display = "none"
+    characterCard.addEventListener("click", () => {
+      // Show characterCard if hidden.  Hide characterCard if shown
+      if (characterInfo.style.display === "none") {
+        characterInfo.style.display = "block" 
+        } else { 
         characterInfo.style.display = "none"
-        characterCard.addEventListener("click", () => {
-            // Show characterCard if hidden.  Hide characterCard if shown
-            if (characterInfo.style.display === "none") {
-                characterInfo.style.display = "block" 
-                } else { 
-                characterInfo.style.display = "none"
-            }
-        })
+        }
+      })
         
-        let characterCardBackground = document.getElementById(`character-card-background`)
-        characterCardBackground.style.display = "none"
-        characterCard.addEventListener("click", () => {
-          characterCardBackground.style.display = "block"
-        })
-        characterCardBackground.addEventListener("click", () => {
-          characterCardBackground.style.display = "none"
-        })
+    let characterCardBackground = document.getElementById(`character-card-background`)
+    characterCardBackground.style.display = "none"
+    characterCard.addEventListener("click", () => {
+      characterCardBackground.style.display = "block"
     })
+    characterCardBackground.addEventListener("click", () => {
+      characterCardBackground.style.display = "none"
+    })
+  })
 }
 
 // Filter characters based on 'film-select' value chosen
 const filterCharacters = () => {
-    const filmSelect = document.getElementById("film-select")
-    const filmName = document.getElementById('film-name')
-    filmSelect.addEventListener("change", () => filmChange())
-    const filmChange = () => {
-        const selectedFilm = filmSelect.value;     
-        switch (selectedFilm) {
-          case "film-all": filmCurrent = filmAllArray   
-            filmName.textContent = "All Films"
-            break      
-          case "film-one": filmCurrent = filmOneArray   
-            filmName.textContent = "The Phantom Menace" 
-            break
-          case "film-two": filmCurrent = filmTwoArray
-            filmName.textContent = "Attack of the Clones"
-            break
-          case "film-three": filmCurrent = filmThreeArray
-            filmName.textContent = "Revenge of the Sith"
-            break
-          case "film-four": filmCurrent = filmFourArray
-            filmName.textContent = "A New Hope"
-            break
-          case "film-five": filmCurrent = filmFiveArray
-            filmName.textContent = "The Empire Strikes Back"
-            break
-          case "film-six": filmCurrent = filmSixArray
-            filmName.textContent = "Return of the Jedi"
-            break
-          default: console.log("THE FORCE IS NOT STRONG WITH US!")
-            break
-        }        
-        loadCharacters(filmCurrent)
+  const filmSelect = document.getElementById("film-select")
+  const filmName = document.getElementById('film-name')
+  filmSelect.addEventListener("change", () => filmChange())
+  const filmChange = () => {
+    const selectedFilm = filmSelect.value     
+    switch (selectedFilm) {
+      case "film-all": filmCurrent = filmAllArray   
+        filmName.textContent = "All Films"
+        break      
+      case "film-one": filmCurrent = filmOneArray   
+        filmName.textContent = "The Phantom Menace" 
+        break
+      case "film-two": filmCurrent = filmTwoArray
+        filmName.textContent = "Attack of the Clones"
+        break
+      case "film-three": filmCurrent = filmThreeArray
+        filmName.textContent = "Revenge of the Sith"
+        break
+      case "film-four": filmCurrent = filmFourArray
+        filmName.textContent = "A New Hope"
+        break
+      case "film-five": filmCurrent = filmFiveArray
+        filmName.textContent = "The Empire Strikes Back"
+        break
+      case "film-six": filmCurrent = filmSixArray
+        filmName.textContent = "Return of the Jedi"
+        break
+      default: console.log("THE FORCE IS NOT STRONG WITH US!")
+        break
+      }        
+      loadCharacters(filmCurrent)
     }
 }  
 
